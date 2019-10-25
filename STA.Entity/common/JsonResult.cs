f6b29b81555f40de0fc894cccae9e28c26cdceaa
Potato <silenceace@gmail.com>
2019-10-25 16:36:24 +0800

    JSON Entity

---
+++ STA.Entity/common/JsonResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace STA.Entity.Common
+{
+    [DataContract]
+    public class JsonResult<T>
+    {
+        public JsonResult() { }
+
+        public JsonResult(int code = 10000, string msg = "success")
+        {
+            Code = code;
+            Msg = msg;
+        }
+
+        [DataMember(Name = "code")]
+        public int Code { get; set; }
+
+        [DataMember(Name = "msg")]
+        public string Msg { get; set; }
+
+        [DataMember(Name = "result")]
+        public T Result { get; set; }
+    }
+}
